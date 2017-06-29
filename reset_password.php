cf75ad30a33f2a98fcf0b9153597919e3438ca99
Gianni996 <giammaria.massafra@gmail.com>
2017-06-29 19:38:32 +0200

    a

---
+++ reset_password.php
@@ -0,0 +1,40 @@
+<?php
+/* Password reset process, updates database with new user password */
+require 'db.php';
+session_start();
+
+
+// Make sure the form is being submitted with method="post"
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+   
+    // Make sure the two passwords match
+    if ( $_POST['newpassword'] == $_POST['confirmpassword'] ) { 
+
+        $new_password = password_hash($_POST['newpassword'], PASSWORD_BCRYPT);
+        
+        // We get $_POST['email'] and $_POST['hash'] from the hidden input field of reset.php form
+        $email = $mysqli->escape_string($_POST['email']);
+        $hash = $mysqli->escape_string($_POST['hash']);
+        
+        //settype($offset, 'integer');
+        //$sql = "UPDATE users SET password='$new_password', hash='$hash' WHERE email='$email'";
+        
+        $sql= sprintf("UPDATE users SET password='$new_password', hash='$hash' WHERE email='$email' %d;", $offset);
+        $sql1=sprintf("UPDATE users SET password='%s', hash='%s' WHERE email='%s'", 
+                mysql_real_escape_string($new_password),
+                mysql_real_escape_string($hash),
+                mysql_real_escape_string($email));
+        if ( $mysql_query($sql1) ) {
+
+        $_SESSION['message'] = 'Your password has been reset successfully!';
+        header('location: success.php');    
+
+        }
+
+    }
+    else {
+        $_SESSION['message'] = "Two passwords you entered don't match, try again!";
+        header('location: error.php');    
+    }
+
+}
